24169b46a60910adb5420e87fdb93cb6fe803ff6
David Flajole <dmfinfo@pobox.com>
2023-10-02 00:20:05 -0700

    replaced unsafe to_atom

--- config/test.exs
+++ config/test.exs
@@ -8,8 +8,7 @@ config :grub_club, GrubClubWeb.Endpoint,
   server: false
 
 # In test we don't send emails.
-config :grub_club, GrubClub.Mailer,
-  adapter: Swoosh.Adapters.Test
+config :grub_club, GrubClub.Mailer, adapter: Swoosh.Adapters.Test
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
--- lib/grub_club/food_carts/food_carts.ex
+++ lib/grub_club/food_carts/food_carts.ex
@@ -2,12 +2,23 @@ defmodule GrubClub.FoodCarts do
   def get_food_carts() do
     File.stream!("assets/MFFP.csv")
     |> CSV.decode(headers: true)
-    |> Enum.to_list
-    |> Enum.map(&elem(&1,1))
-    |> Enum.filter(&String.length(&1["Zip Codes"]) == 5)
-    |> Enum.map(&(Map.take(&1, ["Applicant", "Address", "Latitude", "Longitude", "Zip Codes"])))
-    |> Enum.map(&(Map.new(&1, fn x -> {String.downcase(elem(x, 0)), elem(x, 1)} end)))
-    |> Enum.map(&(Map.new(&1, fn x -> {String.replace(elem(x, 0), " ", "_"), elem(x, 1)} end)))
-    |> Enum.map(&(Map.new(&1, fn x -> {String.to_atom(elem(x, 0)), elem(x, 1)} end)))
+    |> Enum.to_list()
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.filter(&(String.length(&1["Zip Codes"]) == 5))
+    |> Enum.map(&Map.take(&1, ["Applicant", "Address", "Latitude", "Longitude", "Zip Codes"]))
+    |> Enum.map(&Map.new(&1, fn x -> {String.downcase(elem(x, 0)), elem(x, 1)} end))
+    |> Enum.map(&Map.new(&1, fn x -> {String.replace(elem(x, 0), " ", "_"), elem(x, 1)} end))
+    |> Enum.map(
+      &Map.new(&1, fn x -> {String.replace(elem(x, 0), "zip_codes", "zip_code"), elem(x, 1)} end)
+    )
+    |> Enum.map(
+      &Map.new([
+        {:applicant, &1["applicant"]},
+        {:address, &1["address"]},
+        {:latitude, &1["latitude"]},
+        {:longitude, &1["longitude"]},
+        {:zip_code, &1["zip_code"]}
+      ])
+    )
   end
 end
